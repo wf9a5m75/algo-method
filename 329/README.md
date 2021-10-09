453292dfe3134a17060cdeeaf9dd2839bd9a779d
masashi <wf9a5m75@gmail.com>
2021-10-09 15:22:51 -0700

    solve : Q2-4. コマの移動 (1)

---
+++ 329/README.md
@@ -0,0 +1,2 @@
+# Q2-4. コマの移動 (1)
+https://algo-method.com/tasks/329
---
+++ 329/solution.py
@@ -0,0 +1,75 @@
+from io import StringIO
+import sys
+
+class Solution:
+    def solve(self, N):
+
+        # 1
+        #
+        # 1 1
+        # 1 2
+        #
+        # 1 1 1
+        # 1 2 3
+        # 1 3 6
+        dp = [[0] * N for _ in range(N)]
+
+
+        for i in range(N):
+            dp[i][0] = dp[0][i] = 1
+
+        for y in range(1, N):
+            for x in range(1, N):
+                dp[y][x] = dp[y - 1][x] + dp[y][x - 1]
+
+        # for y in range(N):
+        #     print(dp[y])
+        return dp[N - 1][N - 1]
+
+def main():
+    N = int(input())
+    print(Solution().solve(N))
+
+def test(testName, inputFile, outputFile):
+    capture = StringIO()
+    sys.stdin = open(inputFile)
+    sys.stdout = capture
+    main()
+    sys.stdin = sys.__stdin__
+    sys.stdout = sys.__stdout__
+    results = capture.getvalue().split("\n")
+
+
+    fAnsFile = open(outputFile)
+    i = 0
+    passTheTest = True
+    lenResults = len(results)
+    for ansLine in fAnsFile:
+        ansLine = ansLine.strip()
+        if ansLine != results[i]:
+            results[i] = "-->|" + results[i]
+            passTheTest = False
+        else:
+            results[i] = "   |" + results[i]
+
+        i += 1
+        if i == lenResults:
+            break
+    if ((not passTheTest) or (i + 1 != len(results))):
+        print("[{0}] -> fail".format(testName))
+        print("\n".join(results))
+    else:
+        print("[{0}] -> pass".format(testName))
+
+    fAnsFile.close()
+    capture.close()
+
+def test1():
+    test("test1", "input01.txt", "output01.txt")
+
+def test0():
+    test("test0", "input00.txt", "output00.txt")
+
+if __name__ == "__main__":
+    test0()
+    test1()
