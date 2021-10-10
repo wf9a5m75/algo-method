561e787c425702a21b78ec8c0b7afde89b1cf151
masashi <wf9a5m75@gmail.com>
2021-10-10 00:15:32 -0700

    solve:  数字の全探索 1

---
+++ 220/README.md
@@ -0,0 +1,2 @@
+# 数字の全探索 1
+https://algo-method.com/tasks/220
---
+++ 220/solution.py
@@ -0,0 +1,60 @@
+from io import StringIO
+import sys
+
+class Solution:
+	def solve(self, N):
+		if (N <= 5):
+			return 1
+		ans = 0
+		for i in range(1, N + 1):
+			if i % 5 != 0 and i % 2 != 0 and i % 3 != 0:
+				ans += 1
+		return ans
+
+def main():
+    N = int(input())
+    print(Solution().solve(N))
+
+def test(testName, inputFile, outputFile):
+    capture = StringIO()
+    sys.stdin = open(inputFile)
+    sys.stdout = capture
+    main()
+    sys.stdin = sys.__stdin__
+    sys.stdout = sys.__stdout__
+    results = capture.getvalue().split("\n")
+
+
+    fAnsFile = open(outputFile)
+    i = 0
+    passTheTest = True
+    lenResults = len(results)
+    for ansLine in fAnsFile:
+        ansLine = ansLine.strip()
+        if ansLine != results[i]:
+            results[i] = "-->|" + results[i]
+            passTheTest = False
+        else:
+            results[i] = "   |" + results[i]
+
+        i += 1
+        if i == lenResults:
+            break
+    if ((not passTheTest) or (i + 1 != len(results))):
+        print("[{0}] -> fail".format(testName))
+        print("\n".join(results))
+    else:
+        print("[{0}] -> pass".format(testName))
+
+    fAnsFile.close()
+    capture.close()
+
+def test1():
+    test("test1", "input01.txt", "output01.txt")
+
+def test0():
+    test("test0", "input00.txt", "output00.txt")
+
+if __name__ == "__main__":
+    test0()
+    test1()
