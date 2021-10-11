a1c523ecdbb26bc0447ccb2891e1c9f75770162e
masashi <wf9a5m75@gmail.com>
2021-10-10 17:27:24 -0700

    solve: 数字の全探索 3

---
+++ 222/README.md
@@ -0,0 +1,2 @@
+# 数字の全探索 3
+https://algo-method.com/tasks/222
---
+++ 222/solution.py
@@ -0,0 +1,67 @@
+from io import StringIO
+import sys
+
+import math
+def isPrime(N):
+	if N == 1:
+		return "No"
+	if N < 4:
+		return "Yes"
+	if (N % 2 == 0) or (N % 3 == 0):
+		return "No"
+	i = 5
+	upper = math.sqrt(N)
+
+	while(i <= upper):
+		if (N % i == 0) or (N % (i + 2) == 0):
+			return "No"
+		i += 6
+	return "Yes"
+
+def main():
+    N = int(input())
+    print(isPrime(N))
+
+def test(testName, inputFile, outputFile):
+    capture = StringIO()
+    sys.stdin = open(inputFile)
+    sys.stdout = capture
+    main()
+    sys.stdin = sys.__stdin__
+    sys.stdout = sys.__stdout__
+    results = capture.getvalue().split("\n")
+
+
+    fAnsFile = open(outputFile)
+    i = 0
+    passTheTest = True
+    lenResults = len(results)
+    for ansLine in fAnsFile:
+        ansLine = ansLine.strip()
+        if ansLine != results[i]:
+            results[i] = "-->|" + results[i]
+            passTheTest = False
+        else:
+            results[i] = "   |" + results[i]
+
+        i += 1
+        if i == lenResults:
+            break
+    if ((not passTheTest) or (i + 1 != len(results))):
+        print("[{0}] -> fail".format(testName))
+        print("\n".join(results))
+    else:
+        print("[{0}] -> pass".format(testName))
+
+    fAnsFile.close()
+    capture.close()
+
+def test1():
+    test("test1", "input01.txt", "output01.txt")
+
+def test0():
+    test("test0", "input00.txt", "output00.txt")
+
+if __name__ == "__main__":
+    test0()
+    test1()
