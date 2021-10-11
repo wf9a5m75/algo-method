851e33f53ec1c67ce6510f4ab0ce79197bf3a4f5
masashi <wf9a5m75@gmail.com>
2021-10-11 10:38:03 -0700

    solve: Q1-4. 荷物と箱

---
+++ 361/README.md
@@ -0,0 +1,2 @@
+# Q1-4. 荷物と箱
+https://algo-method.com/tasks/361
---
+++ 361/solution.py
@@ -0,0 +1,71 @@
+from io import StringIO
+import sys
+
+def main():
+    N, M = list(map(int, input().split()))
+    A = list(map(int, input().split()))
+    B = list(map(int, input().split()))
+
+    A.sort()
+    nA = len(A)
+    nB = len(B)
+    dp = [False] * nB
+
+    for a in range(nA - 1, -1, -1):
+        for b in range(nB - 1, -1, -1):
+            if (not dp[b]) and (A[a] <= B[b]):
+                dp[b] = True
+                break
+    cnt = 0
+    for i in range(nB):
+        cnt += 1 if dp[i] else 0
+    print(cnt)
+
+
+def test(testName, inputFile, outputFile):
+    capture = StringIO()
+    sys.stdin = open(inputFile)
+    sys.stdout = capture
+    main()
+    sys.stdin = sys.__stdin__
+    sys.stdout = sys.__stdout__
+    results = capture.getvalue().split("\n")
+
+
+    fAnsFile = open(outputFile)
+    i = 0
+    passTheTest = True
+    lenResults = len(results)
+    for ansLine in fAnsFile:
+        ansLine = ansLine.strip()
+        if ansLine != results[i]:
+            results[i] = "-->|" + results[i]
+            passTheTest = False
+        else:
+            results[i] = "   |" + results[i]
+
+        i += 1
+        if i == lenResults:
+            break
+    if ((not passTheTest) or (i + 1 != len(results))):
+        print("[{0}] -> fail".format(testName))
+        print("\n".join(results))
+    else:
+        print("[{0}] -> pass".format(testName))
+
+    fAnsFile.close()
+    capture.close()
+
+def test2():
+    test("test2", "input02.txt", "output02.txt")
+
+def test1():
+    test("test1", "input01.txt", "output01.txt")
+
+def test0():
+    test("test0", "input00.txt", "output00.txt")
+
+if __name__ == "__main__":
+    test0()
+    test1()
+    # test2()
