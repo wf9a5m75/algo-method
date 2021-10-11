d63db4f9c185b801c13bdf139452821fa61b8b38
masashi <wf9a5m75@gmail.com>
2021-10-11 01:00:24 -0700

    solve: Q1-2. お菓子 (1)

---
+++ 362/README.md
@@ -0,0 +1,2 @@
+# Q1-2. お菓子 (1)
+https://algo-method.com/tasks/362
---
+++ 362/solution.py
@@ -0,0 +1,64 @@
+from io import StringIO
+import sys
+
+def main():
+    N = int(input())
+
+    cnt = 0
+    while(N > 0):
+        if (N & 1 == 1):
+            N -= 1
+            cnt += 1
+        else:
+            N >>= 1
+            cnt += 1
+    print(cnt)
+
+
+def test(testName, inputFile, outputFile):
+    capture = StringIO()
+    sys.stdin = open(inputFile)
+    sys.stdout = capture
+    main()
+    sys.stdin = sys.__stdin__
+    sys.stdout = sys.__stdout__
+    results = capture.getvalue().split("\n")
+
+
+    fAnsFile = open(outputFile)
+    i = 0
+    passTheTest = True
+    lenResults = len(results)
+    for ansLine in fAnsFile:
+        ansLine = ansLine.strip()
+        if ansLine != results[i]:
+            results[i] = "-->|" + results[i]
+            passTheTest = False
+        else:
+            results[i] = "   |" + results[i]
+
+        i += 1
+        if i == lenResults:
+            break
+    if ((not passTheTest) or (i + 1 != len(results))):
+        print("[{0}] -> fail".format(testName))
+        print("\n".join(results))
+    else:
+        print("[{0}] -> pass".format(testName))
+
+    fAnsFile.close()
+    capture.close()
+
+def test2():
+    test("test2", "input02.txt", "output02.txt")
+
+def test1():
+    test("test1", "input01.txt", "output01.txt")
+
+def test0():
+    test("test0", "input00.txt", "output00.txt")
+
+if __name__ == "__main__":
+    test0()
+    test1()
+    # test2()
