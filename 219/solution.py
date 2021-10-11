6e2a462279e302b2ebcca449cefa7b2c2057953c
masashi <wf9a5m75@gmail.com>
2021-10-10 18:02:38 -0700

    solve: 配列の全探索 10

---
+++ 219/solution.py
@@ -0,0 +1,67 @@
+from io import StringIO
+import sys
+
+
+def main():
+    dp = [0] * 10
+    N = int(input())
+    A = list(map(int, input().split()))
+
+    for a in A:
+    	dp[a - 1] += 1
+
+    maxV = 0
+    maxCnt = 0
+    for i in range(10):
+    	if maxCnt < dp[i]:
+    		maxV = i
+    		maxCnt = dp[i]
+    print(maxV + 1)
+
+def test(testName, inputFile, outputFile):
+    capture = StringIO()
+    sys.stdin = open(inputFile)
+    sys.stdout = capture
+    main()
+    sys.stdin = sys.__stdin__
+    sys.stdout = sys.__stdout__
+    results = capture.getvalue().split("\n")
+
+
+    fAnsFile = open(outputFile)
+    i = 0
+    passTheTest = True
+    lenResults = len(results)
+    for ansLine in fAnsFile:
+        ansLine = ansLine.strip()
+        if ansLine != results[i]:
+            results[i] = "-->|" + results[i]
+            passTheTest = False
+        else:
+            results[i] = "   |" + results[i]
+
+        i += 1
+        if i == lenResults:
+            break
+    if ((not passTheTest) or (i + 1 != len(results))):
+        print("[{0}] -> fail".format(testName))
+        print("\n".join(results))
+    else:
+        print("[{0}] -> pass".format(testName))
+
+    fAnsFile.close()
+    capture.close()
+
+def test2():
+    test("test2", "input02.txt", "output02.txt")
+
+def test1():
+    test("test1", "input01.txt", "output01.txt")
+
+def test0():
+    test("test0", "input00.txt", "output00.txt")
+
+if __name__ == "__main__":
+    test0()
+    # test1()
+    # test2()
