61822417eff18057a2f1792c34daf593fb55dc38
masashi <wf9a5m75@gmail.com>
2022-01-03 06:33:01 -0800

    Add another solution: Q3-4. ナップサック問題

--- 341/solution1.py
+++ 341/solution1.py
@@ -1,13 +1,22 @@
-N,M = list(map(int, input().split()))
-A = list(map(int, input().split()))
-B = list(map(int, input().split()))
-dp = [[-1] * M for _ in range(N)]
+N, M = list(map(int, input().split()))
+W = list(map(int, input().split()))
+V = list(map(int, input().split()))
+dp = [[-1] * (M + 1) for _ in range(N + 1)]
 
 dp[0][0] = 0
-for i in range(N - 1):
-	for j in range(M):
-		if dp[i][j] > -1:
-			dp[i + 1][j] = max(dp[i + 1][j], dp[i][j])
-			if (j + A[i] < M):
-				dp[i + 1][j + A[i]] = max(dp[i + 1][j + A[i]], dp[i][j] + B[i])
-print(dp[N - 1][M - 1])
+for i in range(N):
+	for j in range(M + 1):
+		if dp[i][j] == -1:
+			continue
+		# ボールを入れなかった場合
+		dp[i + 1][j] = max(dp[i + 1][j], dp[i][j])
+
+		# ボールを入れた場合
+		if j + W[i] <= M:
+			dp[i + 1][j + W[i]] = max(dp[i + 1][j + W[i]], dp[i][j] + V[i])
+
+ans = max(dp[N])
+if (ans == -1):
+	print(0)
+else:
+	print(ans)
